1bc6532a10482c467df888f2802724c0c19d9436
Matheus Bernardino <123327011+Mathdino@users.noreply.github.com>
2025-02-24 22:24:25 -0300

    feat: add initial cart item component (without funcionallity)

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -30,6 +30,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     setIsOpen((prev) => !prev);
   };
   const addProduct = (product: CartProduct) => {
+    // Verificar se o produto já está no carrinho
+    // Se estiver, aumente a sua quantidade
+    // Se não estiver, adicione-o
     const productIsAlreadyOnTheCart = products.some(
       (prevProduct) => prevProduct.id === product.id,
     );
